50829e7e3dd5d35b9a1eb9e96715266b3774a693
Jonathan <j.ang2@yahoo.com>
2023-08-17 12:31:49 -0700

    added route for all orders

--- app/api/order_routes.py
+++ app/api/order_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
+from flask_login import login_required, current_user
 from app.models import Order, Cupcake, Cookie, Cheesecake
 
 order_routes = Blueprint('orders', __name__)
@@ -7,7 +7,7 @@ order_routes = Blueprint('orders', __name__)
 
 @order_routes.route('/')
 @login_required
-
-
-
-
+def get_all_orders() {
+    orders = Order.query.all()
+    return {"Orders": [order.to_dict() for order in orders]}
+}
