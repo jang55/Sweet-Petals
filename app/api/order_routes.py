161c538f3749b54a86e04587e05c09dcab88e812
Jonathan <j.ang2@yahoo.com>
2023-08-17 12:37:29 -0700

    route for get an order detail completed

--- app/api/order_routes.py
+++ app/api/order_routes.py
@@ -7,7 +7,18 @@ order_routes = Blueprint('orders', __name__)
 
 @order_routes.route('/')
 @login_required
-def get_all_orders() {
+def get_all_orders():
     orders = Order.query.all()
     return {"Orders": [order.to_dict() for order in orders]}
-}
+
+
+
+@order_routes.route('/<int:id>')
+@login_required
+def get_order_detail(id):
+    order = Order.query.get(id)
+
+    if order is None:
+        return {"error": "Order not found"}, 404
+
+    return order.to_dict()
