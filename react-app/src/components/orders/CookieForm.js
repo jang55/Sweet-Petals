54825ef75884bfdf1e4d50d03b8a280047ec4959
Jonathan Ang <j.ang2@yahoo.com>
2023-08-19 15:41:15 -0700

    got select field working properly for cookies

--- react-app/src/components/orders/CookieForm.js
+++ react-app/src/components/orders/CookieForm.js
@@ -1,20 +1,39 @@
 import "./css/cookie-form.css"
+import { useState } from "react";
 
 
 function CookieForm() {
+    const [selectedCookie, setSelectedCookie] = useState("")
+
+    console.log(selectedCookie)
 
     return (
         <div className="cookie-form-container">
             <form>
                 <h1>Choose a Cookie!</h1>
-                <label >
+                <label htmlFor="cookie-selection">
                     <p className="cookie-input-label">
                         flavor <span>*</span>
                     </p>
-                    <input
+                    <select 
                     className="cookie-input-field"
-                    ></input>
+                    value={selectedCookie}
+                    onChange={e => setSelectedCookie(e.target.value)}
+                    id="cookie-selection"
+                    defaultValue=""
+                    >
+                        <option disabled value="">Select a Cookie...</option>
+                        <option value="chocolate chip">Chocolate Chip (6)</option>
+                        <option value="peanut butter">Peanut Butter (6)</option>
+                        <option value="snickerdoodle">Snickerdoodle (6)</option>
+                        <option value="ube crinkle">Ube Crinkle (6)</option>
+                        <option value="chocolate crinkle">Chocolate Crinkle (6)</option>
+                        <option value="sugar cookies">Sugar Cookies (6)</option>
+                        <option value="white chocolate macadamia">White Chocolate Macadamia (6)</option>
+                        <option value="chewy M&M cookies">Chewy M&M Cookies (6)</option>
+                    </select>
                 </label>
+                <button type="submit">Add to cart</button>
             </form>
         </div>
     )
