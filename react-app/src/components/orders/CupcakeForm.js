88e92d4b008f098e29bbb83ed0518f24661c49f8
Jonathan Ang <j.ang2@yahoo.com>
2023-08-19 13:46:35 -0700

    started form for cupcakes

--- react-app/src/components/orders/CupcakeForm.js
+++ react-app/src/components/orders/CupcakeForm.js
@@ -1,11 +1,44 @@
-
+import "./css/cupcake-form.css"
 
 
 function CupcakeForm() {
 
     return (
-        <div>
-            CupcakeForm
+        <div className="cupcake-form-container">
+            <form>
+                <h1>Create a Cupcake!</h1>
+                <label >
+                    <p className="form-input-label">
+                        Color One <span>*</span>
+                    </p>
+                    <input required>
+                    </input>
+                </label>
+                <label >
+                    <p className="form-input-label">
+                        Color Two 
+                    </p>
+                    <input></input>
+                </label>
+                <label >
+                    <p className="form-input-label">
+                        Color Three
+                    </p>
+                    <input></input>
+                </label>
+                <label >
+                    <p className="form-input-label">
+                        style <span>*</span>
+                    </p>
+                    <input></input>
+                </label>
+                <label >
+                    <p className="form-input-label">
+                        flavor <span>*</span>
+                    </p>
+                    <input></input>
+                </label>
+            </form>
         </div>
     )
 }
