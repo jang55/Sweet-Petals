7d7a25ae73fb93f234e9d39b03fe80a0bb7a92d6
Jonathan Ang <j.ang2@yahoo.com>
2023-08-20 12:47:54 -0700

    added conditions to color wheels

--- react-app/src/components/orders/CupcakeForm.js
+++ react-app/src/components/orders/CupcakeForm.js
@@ -1,5 +1,5 @@
 import "./css/cupcake-form.css";
-import { useState, Fragment } from "react";
+import { useState, Fragment, useEffect } from "react";
 import Wheel from "@uiw/react-color-wheel";
 import { hsvaToHex } from "@uiw/color-convert";
 
@@ -8,10 +8,23 @@ function CupcakeForm() {
   const [hsvaOne, setHsvaOne] = useState({ h: 214, s: 43, v: 90, a: 1 });
   const [hsvaTwo, setHsvaTwo] = useState({ h: 214, s: 43, v: 90, a: 1 });
   const [hsvaThree, setHsvaThree] = useState({ h: 214, s: 43, v: 90, a: 1 });
-  const [openWheel, setOpenWheel] = useState("false")
+  const [openWheel, setOpenWheel] = useState("");
+  const [addColorTwo, setAddColorTwo] = useState(false);
+  const [addColorThree, setAddColorThree] = useState(false);
 
+  useEffect(() => {
+    document.addEventListener("mouseup", function (event) {
+      const colorWheel = document.getElementsByClassName("wheel-active")[0];
+      console.log(colorWheel);
+      if (event.target != colorWheel && event.target.parentNode != colorWheel) {
+        if (colorWheel && colorWheel.style) {
+          setOpenWheel("");
+        }
+      }
+    });
+  }, []);
 
-  console.log(hsvaOne);
+  // console.log(hsvaOne);
 
   return (
     <div className="cupcake-container">
@@ -22,10 +35,11 @@ function CupcakeForm() {
               <div className="cupcake-container-6">
                 <form>
                   <h1 className="cupcake-h1">Create a Cupcake!</h1>
-                  <p>Choose atleast one color</p>
+                  <p>Choose atleast one color for the frosting</p>
+                  {/* Color one wheel */}
                   <label className="cupcake-color-one">
                     <p className="cupcake-input-label">
-                      Color One <span>*</span>
+                      Color One <span className="required">*</span>
                     </p>
                     {/* <input value={hsvaToHex(hsva)} required className="cupcake-input-field"></input> */}
                     <Fragment>
@@ -36,10 +50,14 @@ function CupcakeForm() {
                           background: hsvaToHex(hsvaOne),
                           cursor: "pointer",
                         }}
-                        onClick={e => setOpenWheel("wheel-one")}
+                        onClick={(e) => setOpenWheel("wheel-one")}
                       ></div>
                       <Wheel
-                        className={`color-wheel-one ${openWheel === "wheel-one" ? ""  : "wheel-not-active"}`}
+                        className={`color-wheel-one ${
+                          openWheel === "wheel-one"
+                            ? "wheel-active"
+                            : "wheel-not-active"
+                        }`}
                         color={hsvaOne}
                         onChange={(color) =>
                           setHsvaOne({ ...hsvaOne, ...color.hsva })
@@ -48,53 +66,84 @@ function CupcakeForm() {
                       />
                     </Fragment>
                   </label>
-                  <label className="cupcake-color-two">
-                    <p className="cupcake-input-label">Color Two</p>
-                    {/* <input className="cupcake-input-field"></input> */}
-                    <Fragment>
-                      <div
-                        style={{
-                          width: "50%",
-                          height: 34,
-                          background: hsvaToHex(hsvaTwo),
-                          cursor: "pointer",
-                        }}
-                        onClick={e => setOpenWheel("wheel-two")}
-                      ></div>
-                      <Wheel
-                        className={`color-wheel-two ${openWheel === "wheel-two" ? ""  : "wheel-not-active"}`}
-                        color={hsvaTwo}
-                        onChange={(color) =>
-                          setHsvaTwo({ ...hsvaTwo, ...color.hsva })
-                        }
-                      />
-                    </Fragment>
-                  </label>
-                  <label className="cupcake-color-three">
-                    <p className="cupcake-input-label">Color Three</p>
-                    {/* <input className="cupcake-input-field"></input> */}
-                    <Fragment>
-                      <div
-                        style={{
-                          width: "50%",
-                          height: 34,
-                          background: hsvaToHex(hsvaThree),
-                          cursor: "pointer",
-                        }}
-                        onClick={e => setOpenWheel("wheel-three")}
-                      ></div>
-                      <Wheel
-                        className={`color-wheel-three ${openWheel === "wheel-three" ? ""  : "wheel-not-active"}`}
-                        color={hsvaThree}
-                        onChange={(color) =>
-                          setHsvaThree({ ...hsvaThree, ...color.hsva })
-                        }
-                      />
-                    </Fragment>
-                  </label>
+                  {/* color two wheel */}
+                  {addColorTwo ? (
+                    <label className="cupcake-color-two">
+                      <p className="cupcake-input-label">Color Two</p>
+                      {/* <input className="cupcake-input-field"></input> */}
+                      <Fragment>
+                        <div
+                          style={{
+                            width: "50%",
+                            height: 34,
+                            background: hsvaToHex(hsvaTwo),
+                            cursor: "pointer",
+                          }}
+                          onClick={(e) => setOpenWheel("wheel-two")}
+                        ></div>
+                        <Wheel
+                          className={`color-wheel-two ${
+                            openWheel === "wheel-two"
+                              ? "wheel-active"
+                              : "wheel-not-active"
+                          }`}
+                          color={hsvaTwo}
+                          onChange={(color) =>
+                            setHsvaTwo({ ...hsvaTwo, ...color.hsva })
+                          }
+                        />
+                      </Fragment>
+                      <span className="remove" onClick={e => setAddColorTwo(false)}>remove</span>
+                    </label>
+                  ) : (
+                    <div
+                      className="add-color"
+                      onClick={(e) => setAddColorTwo(true)}
+                    >
+                      Add another color
+                    </div>
+                  )}
+                  {/* Color three wheel */}
+                  {addColorThree ? (
+                    <label className="cupcake-color-three">
+                      <p className="cupcake-input-label">Color Three</p>
+                      {/* <input className="cupcake-input-field"></input> */}
+                      <Fragment>
+                        <div
+                          style={{
+                            width: "50%",
+                            height: 34,
+                            background: hsvaToHex(hsvaThree),
+                            cursor: "pointer",
+                          }}
+                          onClick={(e) => setOpenWheel("wheel-three")}
+                        ></div>
+                        <Wheel
+                          className={`color-wheel-three ${
+                            openWheel === "wheel-three"
+                              ? "wheel-active"
+                              : "wheel-not-active"
+                          }`}
+                          color={hsvaThree}
+                          onChange={(color) =>
+                            setHsvaThree({ ...hsvaThree, ...color.hsva })
+                          }
+                        />
+                      </Fragment>
+                      <span className="remove" onClick={e => setAddColorThree(false)}>remove</span>
+                    </label>
+                  ) : (addColorTwo &&
+                    <div
+                      className="add-color"
+                      onClick={(e) => setAddColorThree(true)}
+                    >
+                      Add another color
+                    </div>
+                  )}
+
                   <label>
                     <p className="cupcake-input-label">
-                      style <span>*</span>
+                      style <span className="required">*</span>
                     </p>
                     <select
                       className="cupcake-select-field"
@@ -113,7 +162,7 @@ function CupcakeForm() {
                   </label>
                   <label>
                     <p className="cupcake-input-label">
-                      flavor <span>*</span>
+                      flavor <span className="required">*</span>
                     </p>
                     <select
                       className="cupcake-select-field"
