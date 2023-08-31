28769638d68d248fb10386cd5a3b3afbc4eee40b
Jonathan Ang <j.ang2@yahoo.com>
2023-08-30 20:57:06 -0700

    added gradient to nav bar

--- react-app/src/components/navigation/Navigation.js
+++ react-app/src/components/navigation/Navigation.js
@@ -1,92 +1,109 @@
-import React, { useContext } from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { useContext } from "react";
+import { NavLink, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import { RxHamburgerMenu } from "react-icons/rx";
-import { RiShoppingCart2Line } from "react-icons/ri"
-import { useState} from 'react';
-import { InfoContext } from "../../context/InfoContext" 
-import logo from "../../images/SWEET_PETALS_wlogo.png" 
-import { logout } from '../../store/session';
-import ShoppingCart from '../shopping-cart/ShoppingCart';
-import LoginFormModal from '../modal-pages/LoginFormModal';
-import "./nav.css"
+import { RiShoppingCart2Line } from "react-icons/ri";
+import { useState } from "react";
+import { InfoContext } from "../../context/InfoContext";
+import logo from "../../images/SWEET_PETALS_wlogo.png";
+import { logout } from "../../store/session";
+import ShoppingCart from "../shopping-cart/ShoppingCart";
+import LoginFormModal from "../modal-pages/LoginFormModal";
+import "./nav.css";
 import { removeAllCartItems } from "../../store/cartReducer";
 
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
+  const { cartCount, openShoppingCart, setOpenShoppingCart } =
+    useContext(InfoContext);
+  const [openMenu, setOpenMenu] = useState(false);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
-    const { cartCount, openShoppingCart, setOpenShoppingCart } = useContext(InfoContext);
-    const [openMenu, setOpenMenu] = useState(false);
-    const dispatch = useDispatch()
-    const history = useHistory();
-    
-    // useEffect(() => {
-    //   if(!sessionUser) {
-    //     return history.push("/")
-    //   }
-    // }, [sessionUser])
+  // useEffect(() => {
+  //   if(!sessionUser) {
+  //     return history.push("/")
+  //   }
+  // }, [sessionUser])
 
-    const toggleMenu = () => {
-        setOpenMenu(!openMenu);
-    }
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
 
-    const toggleShoppingCart = () => {
-        setOpenShoppingCart(!openShoppingCart);
-    }
+  const toggleShoppingCart = () => {
+    setOpenShoppingCart(!openShoppingCart);
+  };
 
-    const logoutHandler = async () => {
-        await dispatch(logout());
-        // await dispatch(removeAllCartItems());
-        return history.push('/');
-    }
+  const logoutHandler = async () => {
+    await dispatch(logout());
+    // await dispatch(removeAllCartItems());
+    return history.push("/");
+  };
 
-	return (
-        <div className='nav-outer-container'>
-            <div className='nav-container'>
-                <NavLink to="/" className="nav-logo-wrapper">
-                    <img src={logo} alt='cupcake-logo'className='nav-logo'/>
-                </NavLink>
-                <ul className='nav-list-wrapper'>
-                    {/* <li>
-                        <NavLink exact to="/">Home</NavLink>
-                    </li> */}
-                    <li>
-                        <NavLink exact to="/reviews/users">Reviews</NavLink>
-                    </li>
-                    {sessionUser && <li>
-                        <NavLink exact to="/orders/new">Create Order</NavLink>
-                    </li>}
-                    {/* <div><LoginFormModal /></div> */}
-                    {/* <div><SignupFormModal /></div> */}
-                    
-                    {(!sessionUser ? <div><LoginFormModal /></div> :
-                        (isLoaded && <li 
-                        className='nav-menu-wrapper'
-                        onClick={toggleMenu}
-                        >
-                            <RxHamburgerMenu className='nav-menu-button'/>
-                            {openMenu && 
-                            <div className='nav-menu'>
-                                {/* <div onClick={e => loginModal()}>Login</div>
-                                <div onClick={e => signupModal()}>Signup</div> */}
-                                <NavLink to="/orders">All orders</NavLink>
-                                <NavLink to="/orders/users">My orders</NavLink>
-                                <div onClick={logoutHandler}>Logout</div>
-                            </div>}
-                        </li>)
-                    )}
-                    <li className='nav-shopping-wrapper'>
-                        <RiShoppingCart2Line 
-                        className='nav-shopping-cart' 
-                        onClick={toggleShoppingCart}
-                        />
-                        {cartCount >= 1 && <div className='nav-shopping-count'>{cartCount}</div>}
-                        {openShoppingCart && <div className='nav-shopping-cart-items'><ShoppingCart /></div>}
-                    </li>
-                </ul>
+  return (
+    <div className="nav-outer-container">
+      <div className="nav-container">
+        <NavLink to="/" className="nav-logo-wrapper">
+          <img src={logo} alt="cupcake-logo" className="nav-logo" />
+        </NavLink>
+        <ul className="nav-list-wrapper">
+          {sessionUser && (
+            <li>
+              <NavLink exact to="/orders/new">
+                Create Order
+              </NavLink>
+            </li>
+          )}
+{/**************** sections for login button and navigations ********************************/}
+          {!sessionUser ? (
+            <div>
+              <LoginFormModal />
             </div>
-        </div>
-	);
+          ) : (
+            isLoaded && (
+              <li className="nav-menu-wrapper" onClick={toggleMenu}>
+                <RxHamburgerMenu className="nav-menu-button" />
+                {openMenu && (
+                  <ul className="nav-menu">
+                    
+                    {sessionUser.role === "admin" && 
+                    <li>
+                      <NavLink to="/orders">All orders</NavLink>
+                    </li>}
+                    <li>
+                      <NavLink to="/orders/users">My orders</NavLink>
+                    </li>
+                    <li>
+                      <NavLink exact to="/reviews/users">
+                        My reviews
+                      </NavLink>
+                    </li>
+                    <div onClick={logoutHandler}>Logout</div>
+                  </ul>
+                )}
+              </li>
+            )
+          )}
+{/**************** end sections for login button and navigations ********************************/}
+
+          <li className="nav-shopping-wrapper">
+            <RiShoppingCart2Line
+              className="nav-shopping-cart"
+              onClick={toggleShoppingCart}
+            />
+            {cartCount >= 1 && (
+              <div className="nav-shopping-count">{cartCount}</div>
+            )}
+            {openShoppingCart && (
+              <div className="nav-shopping-cart-items">
+                <ShoppingCart />
+              </div>
+            )}
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
