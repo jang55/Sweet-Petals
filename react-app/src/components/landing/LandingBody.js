c2648af57bc9fa6fe5c1f265267e7a8aaf005432
Jonathan Ang <j.ang2@yahoo.com>
2023-09-02 14:54:22 -0700

    added some images for each sections of the landing page

--- react-app/src/components/landing/LandingBody.js
+++ react-app/src/components/landing/LandingBody.js
@@ -1,50 +1,74 @@
 import "./landing-css/landing-body.css";
 import InfiniteCarousel from "react-leaf-carousel";
-import logo from "../../images/SWEET_PETALS_wlogo.png" 
-import img_1 from "../../images/IMG_1.JPEG"
-import img_2 from "../../images/IMG_2.JPEG"
-import img_3 from "../../images/IMG_3.JPEG"
-import img_4 from "../../images/IMG_4.JPEG"
-import img_5 from "../../images/IMG_5.JPEG"
-import img_6 from "../../images/IMG_6.JPEG"
-import img_7 from "../../images/IMG_7.JPEG"
-import img_8 from "../../images/IMG_8.JPEG"
-import img_9 from "../../images/IMG_9.JPEG"
-import img_10 from "../../images/IMG_10.JPEG"
-import img_11 from "../../images/IMG_11.JPEG"
-
+import logo from "../../images/SWEET_PETALS_wlogo.png";
+import img_1 from "../../images/IMG_1.JPEG";
+import img_2 from "../../images/IMG_2.JPEG";
+import img_3 from "../../images/IMG_3.JPEG";
+import img_4 from "../../images/IMG_4.JPEG";
+import img_5 from "../../images/IMG_5.JPEG";
+import img_6 from "../../images/IMG_6.JPEG";
+import img_7 from "../../images/IMG_7.JPEG";
+import img_8 from "../../images/IMG_8.JPEG";
+import img_9 from "../../images/IMG_9.JPEG";
+import img_10 from "../../images/IMG_10.JPEG";
+import img_11 from "../../images/IMG_11.JPEG";
 
 function LandingBody() {
   return (
     <div className="landing-main-body-container">
+      {/* ****** section 1 ******* */}
       <div className="landing-body-wrapper-1"></div>
       {/* ****** section 2 ******* */}
       <div className="landing-body-wrapper-2">
         <div className="lb-wrap-2">
-            <img src={logo} className="lb-2-img"/>
-            <h2 className="lb-2-h2">A quick about me about here</h2>
-            <p className="lb-2-p">A random paragraph place holder for people to see so that im going to keep typing in here to make this very long just to have some info space</p>
+          <img src={logo} className="lb-2-img" />
+          <h2 className="lb-2-h2">A quick about me about here</h2>
+          <p className="lb-2-p">
+            A random paragraph place holder for people to see so that im going
+            to keep typing in here to make this very long just to have some info
+            space
+          </p>
         </div>
       </div>
-            {/* ****** section 3 ******* */}
+      {/* ****** section 3 ******* */}
       <div className="landing-body-wrapper-3">
         <div className="lb-wrap-3">
-            <img src={logo} className="lb-3-img"/>
-            <h2 className="lb-3-h2">A quick description about one dessert</h2>
-            <p className="lb-3-p">A random paragraph place holder for people to see so that im going to keep typing in here to make this very long just to have some info space</p>
+          <img src={"https://i.imgur.com/JBirxgX.jpg"} className="lb-3-img" />
+          <h2 className="lb-3-h2">A quick description about one dessert</h2>
+          <p className="lb-3-p">
+            A random paragraph place holder for people to see so that im going
+            to keep typing in here to make this very long just to have some info
+            space
+          </p>
         </div>
       </div>
-            {/* ****** section 4 ******* */}
+      {/* ****** section 4 ******* */}
       <div className="landing-body-wrapper-4">
         <div className="lb-wrap-4">
-            <img src={logo} className="lb-4-img"/>
-            <h2 className="lb-4-h2">A quick description about one dessert</h2>
-            <p className="lb-4-p">A random paragraph place holder for people to see so that im going to keep typing in here to make this very long just to have some info space</p>
+          <img src={img_5} className="lb-4-img" />
+          <h2 className="lb-4-h2">A quick description about one dessert</h2>
+          <p className="lb-4-p">
+            A random paragraph place holder for people to see so that im going
+            to keep typing in here to make this very long just to have some info
+            space
+          </p>
         </div>
       </div>
-      {/* ********** section 5 ************** */}
+      {/* ****** section 5 ******* */}
       <div className="landing-body-wrapper-5">
-        <h2 className="lb-5-h2">Some of our creations</h2>
+        <div className="lb-wrap-5">
+          <img src={"https://i.imgur.com/HhxM6ko.jpg"} className="lb-5-img" />
+          <h2 className="lb-5-h2">A quick description about one dessert</h2>
+          <p className="lb-5-p">
+            A random paragraph place holder for people to see so that im going
+            to keep typing in here to make this very long just to have some info
+            space
+          </p>
+        </div>
+      </div>
+      {/* ********** section 6 ************** */}
+      <div className="landing-body-wrapper-6">
+        <h2 className="lb-6-h2">Some of our creations</h2>
         <div className="landing-carousel-wrap">
           <InfiniteCarousel
             className="landing-carousel"
@@ -76,46 +100,22 @@ function LandingBody() {
             // arrows={false}
           >
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_1}
-              />
+              <img className="landing-images" alt="" src={img_1} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_2}
-              />
+              <img className="landing-images" alt="" src={img_2} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_3}
-              />
+              <img className="landing-images" alt="" src={img_3} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_4}
-              />
+              <img className="landing-images" alt="" src={img_4} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_5}
-              />
+              <img className="landing-images" alt="" src={img_5} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_6}
-              />
+              <img className="landing-images" alt="" src={img_6} />
             </div>
             {/* <div>
               <img
@@ -139,18 +139,10 @@ function LandingBody() {
               />
             </div> */}
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_10}
-              />
+              <img className="landing-images" alt="" src={img_10} />
             </div>
             <div>
-              <img
-                className="landing-images"
-                alt=""
-                src={img_11}
-              />
+              <img className="landing-images" alt="" src={img_11} />
             </div>
           </InfiniteCarousel>
         </div>
