87472d27d8cac048a391c0c62db846523507e918
Jonathan Ang <j.ang2@yahoo.com>
2023-09-14 22:35:59 -0700

    added error message to user search

--- react-app/src/components/messages/MessageList.js
+++ react-app/src/components/messages/MessageList.js
@@ -1,26 +1,34 @@
 import "./css/message-page.css";
+import "./css/message-list.css"
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { getAllMessagesThunk } from "../../store/messageReducer";
-
 import { dateFormatThree } from "../../utils/helperFunctions";
 import { useHistory } from "react-router-dom";
+import { getAllUsersThunk } from "../../store/userReducer"
 
 function MessageList() {
   const dispatch = useDispatch();
   const history = useHistory();
   const user = useSelector((state) => state.session.user);
+  const allUsers = useSelector((state) => state.userState);
   const allMessages = useSelector((state) => state.messageState);
   const [isLoaded, setIsLoaded] = useState(false);
   const [userMessages, setUserMessages] = useState([]);
+  const [customerEmail, setCustomerEmail] = useState("");
+  const [errors, setErrors] = useState({});
+
 
   useEffect(() => {
     if (user && user.role === "admin") {
       dispatch(getAllMessagesThunk()).then(() => {
         setIsLoaded(true);
       });
+      dispatch(getAllUsersThunk());
     }
   }, [user]);
+
+
 
   useEffect(() => {
     if (user && user.role === "admin") {
@@ -28,11 +36,38 @@ function MessageList() {
     }
   }, [user, allMessages]);
 
+
+
+  useEffect(() => {
+    if(Object.values(errors).length > 0) {
+      setErrors({});
+    }
+  }, [customerEmail])
+
+
+
   const handleOpenChat = async (e, customerId) => {
     e.preventDefault();
 
     history.push(`/messages/users/${customerId}`);
   };
+
+
+
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    const customer = Object.values(allUsers).find(user => user.email === customerEmail)
+
+    if(customer) {
+      history.push(`/messages/users/${customer.id}`);
+      return;
+    } else {
+      const errors = {};
+      errors["email"] = "Customer not found";
+      setErrors({...errors})
+    }
+    
+  }
 
   return (
     isLoaded && user && user.role === "admin" && (
@@ -41,6 +76,11 @@ function MessageList() {
         <p className="message-info-text">
           Your current messages with customers.
         </p>
+        <form onSubmit={handleSearchUser}>
+          <input type="text" value={customerEmail} onChange={e => setCustomerEmail(e.target.value)} placeholder="example@aa.io"></input>
+          <button type="submit">Search</button>
+        </form>
+        {errors && errors.email && <p className="message-list-error">*{errors.email}</p>}
         <div className="message-users-container">
           {userMessages.map((messages, i) => {
             const date = new Date(
